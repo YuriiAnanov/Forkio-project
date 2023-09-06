3ec55843bbfff129d450deed75e8d482071bac18
GarikVodila <vladislavbelyj12@gmail.com>
2023-09-06 14:17:12 +0300

    cheanhe

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,18 @@
 let menuBtn = document.querySelector('.menu_btn');
 let menuList = document.querySelector('.menu_list');
+let body = document.querySelector ('.hero_container')
 
 menuBtn.addEventListener('click', function(e){
-	e.preventDefault();
+	e.stopPropagation();
 	menuBtn.classList.toggle('active');
 	menuList.classList.toggle('active');
+});
+
+body.addEventListener ('click', function(e){
+	menuList.classList.contains ('active');
+
+	if (menuList.classList.contains ('active') === true) {
+		menuList.classList.remove ('active')
+		menuBtn.classList.remove ('active')
+	}
 });
